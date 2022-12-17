09a636c234d6d5a5cc0da45ed54e15838b1862bf
alidihaw <aly.dihaw96@gmail.com>
2022-12-17 22:37:50 +0700

    add checkVersion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AppData } from './app.data';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,43 @@ import { AppData } from './app.data';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  constructor(public appData: AppData) {
+  constructor(
+    public appData: AppData,  
+    private swUpdate: SwUpdate
+  ) {
   }
 
   ngOnInit(): void {
+    this.checkVersion();
+  }
 
+  checkVersion() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe(async (evt) => {
+          switch (evt.type) {
+              case 'VERSION_DETECTED':
+                  console.log(
+                      `Downloading new app version: ${evt.version.hash}`
+                  );
+                  break;
+              case 'VERSION_READY':
+                  console.log(
+                      `Current app version: ${evt.currentVersion.hash}`
+                  );
+                  console.log(
+                      `New app version ready for use: ${evt.latestVersion.hash}`
+                  );
+                  await this.swUpdate.activateUpdate();
+                  document.location.reload();
+                  break;
+              case 'VERSION_INSTALLATION_FAILED':
+                  console.log(
+                      `Failed to install app version '${evt.version.hash}': ${evt.error}`
+                  );
+                  break;
+          }
+      });
+    }
   }
 
   ngOnDestroy(): void {
