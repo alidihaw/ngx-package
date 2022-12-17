e7ae96381be044b27a169af78bec8f689fbfd8a7
alidihaw <aly.dihaw96@gmail.com>
2022-12-17 22:10:18 +0700

    add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,7 @@ import { NavbarModule } from '@layouts/navbar/navbar.module';
 import { FooterModule } from '@layouts/footer/footer.module';
 import { CopyrightModule } from '@layouts/copyright/copyright.module';
 import { FormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,12 @@ import { FormsModule } from '@angular/forms';
     NavbarModule,
     FooterModule,
     CopyrightModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/providers/meta.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+
+export interface MetaModel{
+    title : string,
+    desc : string,
+    keywords?: string,
+    type?: string,
+    image?: string
+}
+@Injectable({
+    providedIn: 'root'
+})
+
+export class MetaService {
+
+    constructor(
+        private meta :Meta,
+    ) { }
+
+    setMeta(payload: MetaModel) {
+
+        // Google Format
+        this.meta.updateTag({ name: 'title', content: payload.title, },);
+        this.meta.updateTag({ name: 'description', content: payload.desc, },);
+
+        // Open Graph/ Facebook Format
+        this.meta.updateTag({ name: 'og:title', content: payload.title, },);
+        this.meta.updateTag({ name: 'og:description', content: payload.desc, },);
+        this.meta.updateTag({ name: 'og:type', content: payload.type || 'website' },);
+        this.meta.updateTag({ name: 'og:image', content: payload.image || 'https://storage.googleapis.com/bucket.maple.haus/assets/maple.svg' },);
+        this.meta.updateTag({ name: 'og:url', content: window.location.href },);
+
+        // Twitter Format
+        this.meta.updateTag({ name: 'twitter:card', content: "summary_large_image" },);
+        this.meta.updateTag({ name: 'twitter:title', content: payload.title, },);
+        this.meta.updateTag({ name: 'twitter:description', content: payload.desc, },);
+        this.meta.updateTag({ name: 'twitter:image', content: payload.image || 'https://storage.googleapis.com/bucket.maple.haus/assets/maple.svg' },);
+        this.meta.updateTag({ name: 'twitter:url', content: window.location.href },);
+
+        // General
+        this.meta.updateTag({ name: 'canonical', content: window.location.href },);
+        this.meta.updateTag({ name: 'robots', content: payload.title, });
+        this.meta.updateTag({ name: 'author', content: 'Maple', },);
+
+        if(payload.keywords) {
+            this.meta.updateTag({ name: 'keywords', content: payload.keywords },);
+        }
+    }
+
+
+    
+}
